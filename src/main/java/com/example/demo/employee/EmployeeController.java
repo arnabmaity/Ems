9c1c6713f558f02afb4307891047e0b9c271d395
Deepak Garg <deepak.garg2@globallogic.com>
2018-03-28 17:50:09 +0530

    add Employee post request

--- src/main/java/com/example/demo/employee/EmployeeController.java
+++ src/main/java/com/example/demo/employee/EmployeeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -27,9 +29,20 @@ public class EmployeeController {
 		return myService.getAllEmployees();
 	}
 	
-	@RequestMapping("employees/{EmpId}")
+	@RequestMapping("/employees/{EmpId}")
 	public Employee getEmployee(@PathVariable("EmpId") int id) {
 		return myService.getEmployee(id);
 		//return new Employee(2,"d","d","s");
+	}
+	
+	@RequestMapping(method= RequestMethod.POST,value="/employees")
+	public String addEmployee(@RequestBody Employee employee) {
+		int success= myService.addEmployee(employee);
+		if(success==1) {
+			return "success";
+		}
+		else {
+			return "failure";
+		}
 	}
 }
--- src/main/java/com/example/demo/employee/EmployeeService.java
+++ src/main/java/com/example/demo/employee/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.example.demo.employee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 
-	List<Employee> mylist= Arrays.asList(new Employee(3, "a", "Manager", "address"),
-			new Employee(2, "b", "Employee", "address"));
+	List<Employee> mylist= new ArrayList<Employee>(Arrays.asList(new Employee(3, "a", "Manager", "address"),
+			new Employee(2, "b", "Employee", "address")));
 			
 	public List<Employee> getAllEmployees() {
 		return mylist;
@@ -17,5 +18,16 @@ public class EmployeeService {
 	
 	public Employee getEmployee(int id) {
 		return mylist.stream().filter(t->t.getEmpId()==id).findFirst().get();
+	}
+	
+	public int addEmployee(Employee employee) {
+		int count=mylist.size();
+		mylist.add(employee);
+		if(mylist.size()== count+1) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
 	}
 }
